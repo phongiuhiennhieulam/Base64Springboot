c6e13abf0941a6422ea1726e761f988d29b94cf0
phongiuhiennhieulam <phongdhhe151364@fpt.edu.vn>
2024-05-21 15:32:01 +0700

    update

--- Base64/src/main/java/com/example/base64/controllers/Base64Implement.java
+++ Base64/src/main/java/com/example/base64/controllers/Base64Implement.java
@@ -1,29 +1,28 @@
 package com.example.base64.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.base64.models.StringInput;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Base64;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("base64")
 public class Base64Implement {
-    @GetMapping("decode")
-    public String decode(@RequestParam String input) {
+    @PostMapping(value = "/decode", produces = "application/json", consumes="application/json")
+    public String decode(@RequestBody StringInput input) {
         try {
-            byte[] decodedBytes = Base64.getDecoder().decode(input);
+            byte[] decodedBytes = Base64.getDecoder().decode(input.getInput());
             return new String(decodedBytes);
         }catch (Exception e){
             return e.getMessage();
         }
 
     }
-    @GetMapping("encode")
-    public String encode(@RequestParam String input) {
+    @PostMapping(value = "/encode", produces = "application/json", consumes="application/json")
+    public String encode(@RequestBody StringInput input) {
         try {
-            return Base64.getEncoder().encodeToString(input.getBytes());
+            return Base64.getEncoder().encodeToString(input.getInput().getBytes());
         }catch (Exception e){
             return e.getMessage();
         }
---
+++ Base64/src/main/java/com/example/base64/models/StringInput.java
@@ -0,0 +1,16 @@
+package com.example.base64.models;
+
+public class StringInput {
+    private String input;
+
+    public StringInput() {
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+}
